1de784f4f78cff31407cf649248b761df5827064
koktoh <koktoh96@gmail.com>
2015-12-16 05:15:59 +0900

    基本準備

--- app/src/main/java/com/example/koktoh/smartpetproject/MainActivity.java
+++ app/src/main/java/com/example/koktoh/smartpetproject/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.koktoh.smartpetproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
     }
 }
