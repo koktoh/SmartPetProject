5a6b65e4366ec2b75848018a5615a95c388e0e55
koktoh <koktoh96@gmail.com>
2015-12-16 07:37:24 +0900

    顔画像を表示部分作成

--- app/src/main/java/com/example/koktoh/smartpetproject/MainActivity.java
+++ app/src/main/java/com/example/koktoh/smartpetproject/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.koktoh.smartpetproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
+
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+
+        iv = (ImageView) findViewById(R.id.imageView);
+        iv.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        iv.setImageResource(R.drawable.face_nomal);
     }
 }
