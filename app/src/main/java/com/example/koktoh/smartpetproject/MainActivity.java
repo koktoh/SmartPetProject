161a5e6893df287df5a11408203da2b72b13a5c8
koktoh <koktoh96@gmail.com>
2015-12-16 14:00:02 +0900

    バッテリー情報を取得するように変更 会話パターン追加

--- app/src/main/java/com/example/koktoh/smartpetproject/MainActivity.java
+++ app/src/main/java/com/example/koktoh/smartpetproject/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.koktoh.smartpetproject;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -30,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView iv;
     private Bitmap bmp;
+
+    private int level;
 
     private class MyRecognitionListener implements RecognitionListener {
         @Override
@@ -132,8 +138,8 @@ public class MainActivity extends AppCompatActivity {
                 releaseBitmap();
                 bmp = BitmapFactory.decodeResource(getResources(), R.drawable.face_angry);
                 iv.setImageBitmap(bmp);
-            } else if (resultList.contains("びっくり")) {
-                Log.d(TAG, "びっくり");
+            } else if (resultList.contains("しまった")) {
+                Log.d(TAG, "しまった");
                 releaseBitmap();
                 bmp = BitmapFactory.decodeResource(getResources(), R.drawable.face_oops);
                 iv.setImageBitmap(bmp);
@@ -142,8 +148,24 @@ public class MainActivity extends AppCompatActivity {
                 releaseBitmap();
                 bmp = BitmapFactory.decodeResource(getResources(), R.drawable.face_sleep);
                 iv.setImageBitmap(bmp);
+            } else if (resultList.contains("げんき") || resultList.contains("元気")) {
+                Log.d(TAG, "げんき");
+                releaseBitmap();
+                if (level < 25) {
+                    bmp = BitmapFactory.decodeResource(getResources(), R.drawable.face_oops);
+                    iv.setImageBitmap(bmp);
+                    Toast.makeText(getApplicationContext(), "もうダメ...", Toast.LENGTH_LONG).show();
+                } else if (level < 65) {
+                    bmp = BitmapFactory.decodeResource(getResources(), R.drawable.face_tired);
+                    iv.setImageBitmap(bmp);
+                    Toast.makeText(getApplicationContext(), "ちょっと疲れた", Toast.LENGTH_LONG).show();
+                } else {
+                    bmp = BitmapFactory.decodeResource(getResources(), R.drawable.face_nomal);
+                    iv.setImageBitmap(bmp);
+                    Toast.makeText(getApplicationContext(), "元気！", Toast.LENGTH_LONG).show();
+                }
             }
-            sr.stopListening();
+//            sr.stopListening();
 //            sr.destroy();
 //            sr = null;
 //            sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
@@ -161,6 +183,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
@@ -218,4 +241,113 @@ public class MainActivity extends AppCompatActivity {
             bmp = null;
         }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter filter = new IntentFilter();
+
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(mBroadcastReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(mBroadcastReceiver);
+    }
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                int status = intent.getIntExtra("status", 0);
+                int health = intent.getIntExtra("health", 0);
+                boolean present = intent.getBooleanExtra("present", false);
+                level = intent.getIntExtra("level", 0);
+                int scale = intent.getIntExtra("scale", 0);
+                int icon_small = intent.getIntExtra("icon-small", 0);
+                int plugged = intent.getIntExtra("plugged", 0);
+                int voltage = intent.getIntExtra("voltage", 0);
+                int temperature = intent.getIntExtra("temperature", 0);
+                String technology = intent.getStringExtra("technology");
+
+                String statusString = "";
+
+                switch (status) {
+                    case BatteryManager.BATTERY_STATUS_UNKNOWN:
+                        statusString = "unknown";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_CHARGING:
+                        statusString = "charging";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                        statusString = "discharging";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                        statusString = "not charging";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_FULL:
+                        statusString = "full";
+                        break;
+                }
+
+                String healthString = "";
+
+                switch (health) {
+                    case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                        healthString = "unknown";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_GOOD:
+                        healthString = "good";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                        healthString = "overheat";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_DEAD:
+                        healthString = "dead";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                        healthString = "voltage";
+                        break;
+                    case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                        healthString = "unspecified failure";
+                        break;
+                }
+
+                String acString = "";
+
+                switch (plugged) {
+                    case BatteryManager.BATTERY_PLUGGED_AC:
+                        acString = "plugged ac";
+                        break;
+                    case BatteryManager.BATTERY_PLUGGED_USB:
+                        acString = "plugged usb";
+                        break;
+                }
+
+                if (level == 25) {
+                    releaseBitmap();
+                    bmp = BitmapFactory.decodeResource(getResources(), R.drawable.face_tired);
+                    iv.setImageBitmap(bmp);
+                    Toast.makeText(getApplicationContext(), "お腹空いた...", Toast.LENGTH_LONG).show();
+                }
+
+                Log.v("status", statusString);
+                Log.v("health", healthString);
+                Log.v("present", String.valueOf(present));
+                Log.v("level", String.valueOf(level));
+                Log.v("scale", String.valueOf(scale));
+                Log.v("icon_small", String.valueOf(icon_small));
+                Log.v("plugged", acString);
+                Log.v("voltage", String.valueOf(voltage));
+                Log.v("temperature", String.valueOf(temperature));
+                Log.v("technology", technology);
+            }
+        }
+    };
+
 }
